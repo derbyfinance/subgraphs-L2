58b6586c6b710d93b7f9980efcbedc3beb87ca65
mister_y <sjoerdsommen@gmail.com>
2023-12-21 14:47:24 +0100

    changed and rebuild the graph

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,4 @@
+import { BigInt } from '@graphprotocol/graph-ts';
 import {
   Approval as ApprovalEvent,
   ApprovalForAll as ApprovalForAllEvent,
@@ -11,8 +12,9 @@ import {
   ApprovalForAll,
   BasketId,
   PushProtocolAllocations,
-  RebalanceBasket,
-  Transfer
+  Transfer,
+  Player,
+  Race
 } from "../generated/schema"
 
 export function handleApproval(event: ApprovalEvent): void {
@@ -46,18 +48,25 @@ export function handleApprovalForAll(event: ApprovalForAllEvent): void {
 }
 
 export function handleBasketId(event: BasketIdEvent): void {
-  let entity = new BasketId(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.basketId = event.params.basketId
-  entity.vaultNumber = event.params.vaultNumber
+  let basket = new BasketId(event.params.basketId.toHex());
+  basket.owner = event.params.owner;
+  basket.basketId = event.params.basketId;
+  basket.vaultNumber = event.params.vaultNumber;
+  basket.redeemedRewards = BigInt.fromI32(0);
+  basket.unredeemedRewards = BigInt.fromI32(0);
+  basket.stakedAmount = BigInt.fromI32(0);
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  basket.blockNumber = event.block.number;
+  basket.blockTimestamp = event.block.timestamp;
+  basket.transactionHash = event.transaction.hash;
 
-  entity.save()
+  let player = Player.load(event.params.owner);
+  if (!player) {
+    player = new Player(event.params.owner);
+    player.save();
+  }
+
+  basket.save();
 }
 
 export function handlePushProtocolAllocations(
@@ -78,20 +87,50 @@ export function handlePushProtocolAllocations(
 }
 
 export function handleRebalanceBasket(event: RebalanceBasketEvent): void {
-  let entity = new RebalanceBasket(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.basketId = event.params.basketId
-  entity.rebalancingPeriod = event.params.rebalancingPeriod
-  entity.unredeemedRewards = event.params.unredeemedRewards
-  entity.redeemedRewards = event.params.redeemedRewards
-  entity.deltaAllocations = event.params.deltaAllocations
+  let basket = BasketId.load(event.params.basketId.toHex());
+  if (!basket) return;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  basket.rebalancingPeriod = event.params.rebalancingPeriod;
+  basket.unredeemedRewards = event.params.unredeemedRewards;
+  basket.redeemedRewards = event.params.redeemedRewards;
+  basket.deltaAllocations = event.params.deltaAllocations;
 
-  entity.save()
+  const lastAllocations = basket.allocations
+    ? basket.allocations
+    : new Array<BigInt>(event.params.deltaAllocations.length).fill(BigInt.fromI32(0));
+  const allocations = new Array<BigInt>(event.params.deltaAllocations.length);
+
+  let totalAmount: BigInt = BigInt.fromI32(0);
+
+  for (let i = 0; i < event.params.deltaAllocations.length; i++) {
+    const delta = event.params.deltaAllocations[i];
+    const last = lastAllocations![i] || BigInt.fromI32(0);
+
+    totalAmount = totalAmount.plus(delta);
+    allocations[i] = last.plus(delta);
+  }
+
+  basket.allocations = allocations;
+  basket.stakedAmount = basket.stakedAmount.plus(totalAmount);
+  basket.save();
+
+  if (!basket.vaultNumber) return;
+  let race = Race.load(basket.vaultNumber.toString().concat(event.params.rebalancingPeriod.toHex()));
+  if (!race) {
+    race = new Race(basket.vaultNumber.toString().concat(event.params.rebalancingPeriod.toHex()));
+    race.vaultNumber = basket.vaultNumber;
+  }
+  let totalRewards = race.totalRewards;
+  if (!totalRewards) totalRewards = BigInt.fromI32(0);
+  totalRewards = totalRewards.plus(event.params.unredeemedRewards);
+
+  race.stakedTokens = race.stakedTokens ? race.stakedTokens!.plus(totalAmount) : totalAmount;
+  race.rebalancingPeriod = event.params.rebalancingPeriod;
+  race.apy = BigInt.fromI32(0); // TODO: calculate APY
+  race.totalRewards = totalRewards;
+
+  // allocated tokens
+  race.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
